2ed838015d04734ee41d25d0df2c42bf198be3e0
michaeldyvad <michaeldyvad@gmail.com>
2022-12-01 22:32:54 +0100

    added fetch on index.svelte & logic in directories in server

--- client/src/components/index/Index.svelte
+++ client/src/components/index/Index.svelte
@@ -1,3 +1,18 @@
+<script>
+
+      let residentsArray = []
+
+      fetch("http://localhost:8080/api/residents")
+      .then((res) => res.json())
+      .then((result) => {
+            //residentsArray.push(result[0].residents)
+            result[0].residents.forEach(resident => {
+                  residentsArray.push({name: resident.name, bill: resident.bill})
+            })
+      })
+      console.log(residentsArray)
+</script>
+
 <h1>Welcome to my page</h1>
 <h1>From here you can navigate through the navbar</h1>
 
